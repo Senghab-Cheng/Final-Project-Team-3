e9bcd89aef78450880a92d9d9bc1861a48f30015
Panharoth06 <panharothchheng0903@gmail.com>
2024-09-04 16:21:58 +0700

    Updated : searchEmployee

---
+++ searchEmployee.h
@@ -0,0 +1,51 @@
+#pragma once 
+
+#include <iostream>
+#include <vector>
+#include "addEmployee.h"
+
+using namespace std;
+
+class SearchEmployee{
+    public :
+        string employeeName;
+        int id;
+        string role;
+
+        void searchEmployee() {
+            cout << "[+] Enter employee's name : ";
+            getline(cin, employeeName);
+
+            cout << "[+] Enter employee's ID : ";
+            cin >> id;
+
+            char employeeNameLetter;
+            if (employeeName.length() == 1) {
+                employeeNameLetter = employeeName[0];
+                for (int i = 0; i < employeeNames.size(); i++) {
+                    string name = employeeNames[i];
+                    for (int j = 0; j < employeeNames.size(); j++) {
+                        if (name[j] == employeeNameLetter) {
+                            cout << "Employee's Name : " << name << endl;
+                            cout << "Employee's ID : " << employeeIDs[i] << endl;
+                            cout << "Employee's Role : " << employeeRoles[i] << endl;
+                            cout << "Employee's Salary : " << employeeSalaries[i] << "$" << endl;
+                            break;
+                        }
+                    }
+                }
+            }
+            else {
+                for (int i = 0; i < employeeNames.size(); i++) {
+                    if (employeeName == employeeNames[i]) {
+                        cout << "Employee's Name : " << employeeNames[i] << endl;
+                        cout << "Employee's ID : " << employeeIDs[i] << endl;
+                        cout << "Employee's Role : " << employeeRoles[i] << endl;
+                        cout << "Employee's Salary : " << employeeSalaries[i] << "$" << endl;
+                        break;
+                    }
+                }
+            }
+        } 
+
+};
