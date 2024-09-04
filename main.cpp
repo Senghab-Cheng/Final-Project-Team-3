b45abc5ee8d213e2c69a6c6a85e3e98cb7452039
Panharoth06 <panharothchheng0903@gmail.com>
2024-09-04 14:54:34 +0700

    Updated : addEmployee

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include "userInfo.h"
+#include "addEmployee.h"
 using namespace std;
 
 int main() {
 
+    AddEmployee employee1;
+    employee1.setEmployeeData();
 
     return 0;
 }
