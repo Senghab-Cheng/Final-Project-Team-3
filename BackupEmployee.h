38358b9348c4e84d5e4237cabbc35492ef6541cd
PANHA <panharothchheng0903@gmail.com>
2024-09-09 15:45:22 +0700

    Ey kr ban

--- BackupEmployee.h
+++ BackupEmployee.h
@@ -1,133 +1,150 @@
-#pragma once 
+#pragma once
 
 #include <iostream>
 #include <vector>
 #include "DeleteEmployee.h"
 #include "AddEmployee.h"
+#include "loadingBar.h"
 
 using namespace std;
 
-vector<string> employeeNamesBackup;
-vector<string> employeeRolesBackup;
-vector<int> employeeIDsBackup;
-vector<double> employeeSalariesBackup;
+// Declaration of external backup vectors to avoid multiple definitions
+extern vector<string> employeeNamesBackupX;
+extern vector<string> employeeRolesBackupX;
+extern vector<int> employeeIDsBackupX;
+extern vector<double> employeeSalariesBackupX;
 
 class BackupDataFunctions {
-    public : 
-        virtual void listBackupData() = 0;
-        virtual void emptyData() = 0;
-        virtual void restoreDataByID() = 0;
-        virtual void restoreAllData() = 0;
+public:
+    virtual void listBackupData() = 0;
+    virtual void emptyData() = 0;
+    virtual void restoreDataByID() = 0;
+    virtual void restoreAllData() = 0;
 };
 
-class BackupEmployeeData : private BackupDataFunctions{
-    private :
-        void listBackupData() override{
-            for (int i = 0; i < employeeIDsBackup.size(); i++) {
-                cout << "Name " << employeeNamesBackup[i] << endl;
-                cout << "ID : " << employeeIDsBackup[i] << endl;
-                cout << "Role : " << employeeRolesBackup[i] << endl;
-                cout << "Salary : " << employeeSalariesBackup[i] << "$" << endl << endl;
+class BackupEmployeeData : public BackupDataFunctions {
+private:
+    void listBackupData() override {
+        for (int i = 0; i < employeeIDsBackupX.size(); i++) {
+            cout << "Name: " << employeeNamesBackupX[i] << endl;
+            cout << "ID: " << employeeIDsBackupX[i] << endl;
+            cout << "Role: " << employeeRolesBackupX[i] << endl;
+            cout << "Salary: " << employeeSalariesBackupX[i] << "$" << endl << endl;
+        }
+    }
+
+    void emptyData() override {
+        listBackupData();
+        char choice;
+        cout << "Are you sure you want to empty backup data? [Y/N] : ";
+        cin >> choice;
+        //getLoadingBar(); // LoadingBar
+        if (choice == 'y' || choice == 'Y') {
+            if (employeeIDsBackupX.empty()) {
+                cout << "No backup data available." << endl;
+                return;
             }
+            employeeNamesBackupX.clear();
+            employeeIDsBackupX.clear();
+            employeeRolesBackupX.clear();
+            employeeSalariesBackupX.clear();
+            cout << "Backup data has been successfully emptied." << endl;
+        } else {
+            return;
+        }
+    }
+
+    void restoreDataByID() override {
+        listBackupData();
+        int id;
+        cout << "[+] Enter the employee ID to restore the data: ";
+        cin >> id;
+        //getLoadingBar(); // LoadingBar
+
+        if (employeeIDsBackupX.empty()) {
+            cout << "No backup data available." << endl;
+            return;
         }
 
-        void emptyData() override{
+        auto it = find(employeeIDsBackupX.begin(), employeeIDsBackupX.end(), id);
+        if (it != employeeIDsBackupX.end()) {
+            size_t i = distance(employeeIDsBackupX.begin(), it);
+            employeeNames.push_back(employeeNamesBackupX[i]);
+            employeeIDs.push_back(employeeIDsBackupX[i]);
+            employeeRoles.push_back(employeeRolesBackupX[i]);
+            employeeSalaries.push_back(employeeSalariesBackupX[i]);
+            cout << "Data has been successfully restored." << endl;
+        } else {
+            cout << "Employee with ID " << id << " not found." << endl;
+        }
+    }
+
+    void restoreAllData() override {
+        char choice;
+        cout << "Are you sure you want to restore all data? [Y/N] : ";
+        cin >> choice;
+        if (choice == 'Y' || choice == 'y') {
+            getLoadingBar(); // LoadingBar
+            if (employeeIDsBackupX.empty()) {
+                cout << "No backup data available." << endl;
+                return;
+            }
+
+            for (int i = 0; i < employeeIDsBackupX.size(); i++) {
+                employeeNames.push_back(employeeNamesBackupX[i]);
+                employeeIDs.push_back(employeeIDsBackupX[i]);
+                employeeRoles.push_back(employeeRolesBackupX[i]);
+                employeeSalaries.push_back(employeeSalariesBackupX[i]);
+            }
+            cout << "Data has been successfully restored." << endl;
+        } else {
+            return;
+        }
+    }
+
+    void listMenuBackupData() {
+        cout << "+================================================================================+" << endl;
+        cout << "|                                                                                |" << endl;
+        cout << "|                         >>>  BACKUP DATA MANAGEMENT  <<<                       |" << endl;
+        cout << "|                                                                                |" << endl;
+        cout << "|                                                                                |" << endl;
+        cout << "+================================================================================+" << endl;
+        cout << "|                                                                                |" << endl;
+        cout << "|  [1]  =>  Restore Data by ID                                                   |" << endl;
+        cout << "|  [2]  =>  Restore All Data                                                     |" << endl;
+        cout << "|  [3]  =>  Empty Backup Data                                                    |" << endl;
+        cout << "|  [0]  =>  Exit                                                                 |" << endl;
+        cout << "|                                                                                |" << endl;
+        cout << "+================================================================================+" << endl;
+        cout << "|                >>>  Select an option by entering the number  <<<               |" << endl;
+        cout << "+================================================================================+" << endl;
+    }
+
+public:
+    void backupData() {
+        short option;
+        do {
             listBackupData();
-            char choice;
-            cout << "Are you sure you want to empty backup data? [Y/N] : "; cin >> choice;
-            if (choice == 'y' || choice == 'Y') {
-                employeeNamesBackup.erase(employeeNamesBackup.begin(), employeeNamesBackup.end());
-                employeeIDsBackup.erase(employeeIDsBackup.begin(), employeeIDsBackup.end());
-                employeeRolesBackup.erase(employeeRolesBackup.begin(), employeeRolesBackup.end());
-                employeeSalariesBackup.erase(employeeSalariesBackup.begin(), employeeSalariesBackup.end());
-                cout << "Backup data has been successfully emptied." << endl;
+            cout << "[+] Enter your option: ";
+            cin >> option;
+
+            switch (option) {
+            case 1:
+                restoreDataByID();
+                break;
+            case 2:
+                restoreAllData();
+                break;
+            case 3:
+                emptyData();
+                break;
+            case 0:
+                cout << "Exiting..." << endl;
+                break;
+            default:
+                cout << "INVALID OPTION!" << endl;
+                break;
             }
-            else return;
-        }
-
-        void restoreDataByID() override{
-            listBackupData();
-            int id;
-            cout << "[+] Enter the employee ID to restore the data : "; cin >> id;
-
-            if (id > 0 && id <= employeeIDsBackup.size()) {
-                auto it = find(employeeIDsBackup.begin(), employeeIDsBackup.end(), id);
-                if (it != employeeIDsBackup.end()) {
-                    int i = distance(employeeIDsBackup.begin(), employeeIDsBackup.end());
-                    employeeNames.push_back(employeeNamesBackup[i]);
-                    employeeIDs.push_back(employeeIDsBackup[i]);
-                    employeeRoles.push_back(employeeRolesBackup[i]);
-                    employeeSalaries.push_back(employeeSalariesBackup[i]);
-                    cout << "Data has been successfully restored." << endl;
-                }
-                else cout << "Employee with ID " << id << " not found." << endl;
-            }
-            else cout << "INVALID EMPLOYEE ID!" << endl;  
-        }
-
-        void restoreAllData() override{
-            char choice;
-            cout << "Are you sure you want to restore all data? [Y/N] : "; cin >> choice;
-            if (choice == 'Y' || choice == 'y') {
-                for (int i = 0; i < employeeIDsBackup.size(); i++) {
-                    employeeNames.push_back(employeeNamesBackup[i]);
-                    employeeIDs.push_back(employeeIDsBackup[i]);
-                    employeeRoles.push_back(employeeRolesBackup[i]);
-                    employeeSalaries.push_back(employeeSalariesBackup[i]);
-                    cout << "Data has been successfully restored." << endl;
-                }
-            }
-            else return;
-        }
-
-        void listMenuBackupData() {
-            cout << "=========================================================================" << endl;
-            cout << ">                            | Backup Data |                            <" << endl;
-            cout << ">>=====================================================================<<" << endl;
-            cout << ">>>                                                                   <<<" << endl;
-            cout << ">>>>                      1. Restore Data by ID                      <<<<" << endl;
-            cout << ">>>>>                     2. Restore All Data                       <<<<<" << endl;
-            cout << ">>>>                      3. Empty Data                              <<<<" << endl;
-            cout << ">>>                       0. Exit                                     <<<" << endl;
-            cout << ">>                                                                     <<" << endl;
-            cout << ">                                                                       <" << endl;
-            cout << "=========================================================================" << endl;
-        }
-
-        public :
-
-            void backupData() {
-                short option;
-                do {        
-                    listBackupData();
-                    cout << "[+] Enter your option : "; cin >> option;
-
-                    switch(option) {
-                        case 1 : {
-                            system("cls");
-                            restoreDataByID();
-                            break;
-                        }
-                        case 2 : {
-                            system("cls");
-                            restoreAllData();
-                            break;
-                        }
-                        case 3 : {
-                            system("cls");
-                            emptyData();
-                            break;
-                        }
-                        case 0 : {
-                            cout << "Exiting..." << endl;
-                            break;
-                        }
-                        default : {
-                            cout << "INVALID OPTION!" << endl;
-                            break;
-                        }
-                    }
-
-                } while (option);
-            }
+        } while (option);
+    }
 };
--- main.cpp
+++ main.cpp
@@ -1,88 +1,148 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <ctime>
 
 #include "Login.h"
-#include "AddEmployee.h"
 #include "SearchEmployee.h"
-#include "DeleteEmployee.h"
-#include "Update.h"
 #include "ListEmployee.h"
+#include "AddEmployee.h"
 #include "BackupEmployee.h"
+#include "Update.h"
+#include "DeleteEmployee.h"
+#include "EmployeeManagement.h"
+#include "loadingBar.h"
+#include "Employee.h"
 
 using namespace std;
 
+vector<string> employeeNamesBackupX;
+vector<string> employeeRolesBackupX;
+vector<int> employeeIDsBackupX;
+vector<double> employeeSalariesBackupX;
+
 void listForAdminOrEmployee() {
-    cout << "=========================================================================" << endl;
-    cout << ">                          | Management System |                        <" << endl;
-    cout << ">>=====================================================================<<" << endl;
-    cout << ">>>                                                                   <<<" << endl;
-    cout << ">>>>                                                                 <<<<" << endl;
-    cout << ">>>>>                          1. Admin                             <<<<<" << endl;
-    cout << ">>>>                           2. Employee                           <<<<" << endl;
-    cout << ">>>                            0. Exit                                <<<" << endl;
-    cout << ">>                                                                     <<" << endl;
-    cout << ">                                                                       <" << endl;
-    cout << "=========================================================================" << endl;
+    cout << "+============================================================================+" << endl;
+    cout << "|                                                                            |" << endl;
+    cout << "|                          | MANAGEMENT SYSTEM |                             |" << endl;
+    cout << "|                                                                            |" << endl;
+    cout << "+============================================================================+" << endl;
+    cout << "|                                                                            |" << endl;
+    cout << "|  [1]  =>  Administrator                                                    |" << endl;
+    cout << "|  [2]  =>  Employee Portal                                                  |" << endl;
+    cout << "|  [0]  =>  Exit                                                             |" << endl;
+    cout << "|                                                                            |" << endl;
+    cout << "+============================================================================+" << endl;
+    cout << "|               >>>  Please select an option by entering a number  <<<       |" << endl;
+    cout << "+============================================================================+" << endl;
 }
 
-void listMenu() {
-    cout << "=========================================================================" << endl;
-    cout << ">                                | MENU |                               <" << endl;
-    cout << ">>=====================================================================<<" << endl;
-    cout << ">>>                  1. List Employees                                <<<" << endl;
-    cout << ">>>>                 2. Check Attendance of Employees                <<<<" << endl;
-    cout << ">>>>>                3. Employees Payroll                           <<<<<" << endl;
-    cout << ">>>>                 4. Manipulate Employees                         <<<<" << endl;
-    cout << ">>>                  5. Search Employees                              <<<" << endl;
-    cout << ">>                   6. Profile Management - Setting                   <<" << endl;
-    cout << ">                    0. Exit                                            <" << endl;
-    cout << "=========================================================================" << endl;
+void adminMenu() {
+    cout << "+===========================================================================+" << endl;
+    cout << "|                                                                           |" << endl;
+    cout << "|                                | ADMIN PANEL |                            |" << endl;
+    cout << "|                                                                           |" << endl;
+    cout << "+===========================================================================+" << endl;
+    cout << "|                                                                           |" << endl;
+    cout << "|  [1]  =>  View Employee List                                              |" << endl;
+    cout << "|  [2]  =>  Employee Attendance Tracking                                    |" << endl;
+    cout << "|  [3]  =>  Employee Payroll Management                                     |" << endl;
+    cout << "|  [4]  =>  Employee Management                                             |" << endl;
+    cout << "|  [5]  =>  Search for Employees                                            |" << endl;
+    cout << "|  [6]  =>  Admin Profile Settings                                          |" << endl;
+    cout << "|  [0]  =>  Exit to Main Menu                                               |" << endl;
+    cout << "|                                                                           |" << endl;
+    cout << "+===========================================================================+" << endl;
+    cout << "|            >>>  Please choose an option by entering a number  <<<         |" << endl;
+    cout << "+===========================================================================+" << endl;
+
 }
 
 void ui() {
-    Login admin1;
-    if (admin1.userLogin()) {
+    short op;
+    do {
+    listForAdminOrEmployee();
+        cout << "[+] Enter your option : "; cin >> op;
+        switch(op) {
 
-        system("cls");
-
-        AddEmployee employee1;
-        employee1.setEmployeeData();
-        SearchEmployee employee1a;
-        employee1a.searchEmployee(); 
-
-        ListEmployees employee1b;
-        employee1b.listRoles();   
-        employee1b.listEmployee();
+            // Admin
+            case 1 : {
+                //getLoadingBar();
+                system("cls");
+                Login admin1;
+                if (admin1.userLogin()) {
+                    system("cls");
+                    short option;
+                    do {
+                        adminMenu();
+                        cout << "[+] Enter your option : "; cin >> option;
+                        switch(option) {
+                            case 1 : {
+                                ListEmployees employee1a;
+                                employee1a.listEmployee();
+                                break;
+                            }
+                            case 2 : {
+                                // check attendance employees
+                                break;
+                            }
+                            case 3 : {
+                                // paymentroll employee
+                                break;
+                            }
+                            case 4 : {
+                                Manipulate employee1d;
+                                employee1d.manipulate();
+                                break;
+                            }
+                            case 5 : {
+                                SearchEmployee employee1e;
+                                employee1e.searchEmployee();
+                                break;
+                            }
+                            case 6 : {
+                                // profile - management
+                                break;
+                            }
+                            case 0 : {
+                                cout << "Exting..." << endl;
+                                break;
+                            }
+                            default : {
+                                cout << "INVALID OPTION!" << endl;
+                                break;
+                            }
+                        }
+                    }while(option);
+                } 
+                break;
+            }
+            
+            // Employee
+            case 2 : {
+                // checkIn, checkOut
+                Employee employee1;
+                employee1.checkInCheckOut();
                 
-    }
+                break;
+            }
+            case 0 : {
+                cout << "Exiting..." << endl;
+                break;
+            }
+            default : {
+                cout << "INVALID OPTION!" << endl;
+                break;
+            }
+        }
+    }while(op);  
 }
+  
 
 int main() {
     system("cls");
 
     ui();
-
-    Login l;
-    l.userLogin();
-
-    AddEmployee addem;
-    addem.setEmployeeData();
-
-    SearchEmployee searchem;
-    searchem.searchEmployee();
-
-    DeleteEmployee delete;
-    delete.manage();
-
-    Update u;
-    u.update();
-
-    ListEmployee list;
-    list.listEmployee();
-    list.listRoles();
-
-    BackupEmployee backup;
-    backup.backupData();
-
+    system("pause");
     return 0;
 }
